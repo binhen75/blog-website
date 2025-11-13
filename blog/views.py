202c56ef9056b23ecce8907f722cc53a2c8825c4
binhen75 <binhen.ad@gmail.com>
2025-11-13 17:15:34 +0300

    security

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # Create your views here.
 
 class PostList(ListView):
@@ -12,20 +13,36 @@ class SinglePostView(DetailView):
     model = Post
     template_name="single_post.html"
 
-class BlogCreateView(CreateView):
+class BlogCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = "post_new.html"
-    fields = ["title", "author", "body"]
+    fields = ["title", "body"]
+    def form_valid(self, form):
+        # Attach the currently logged-in user as the author before saving
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
-class UpdatePostView(UpdateView):
+
+class UpdatePostView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     template_name = "post_edit.html"
     fields=["title", "body"]
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+        
     
-class DeletePostView(DeleteView):
+class DeletePostView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = "post_delete.html"
     success_url = reverse_lazy("home")
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
 
 
 
